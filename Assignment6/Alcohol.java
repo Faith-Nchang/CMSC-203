4bc2e020baa6435c03f31ef02d2b956bf46ab189
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2024-03-24 22:05:59 -0400

    Update Alcohol.java

--- Assignment6/Alcohol.java
+++ Assignment6/Alcohol.java
@@ -1,14 +1,6 @@
 /*
- * Class: CMSC203 
- * Instructor: Grigoriy Grinberg
  * Description: Alcohol class - a type of Beverage
- * Due: 12/4/2023
- * Platform/compiler: Eclipse
- * I pledge that I have completed the programming  
- * assignment independently. I have not copied the code 
- * from a student or any source. I have not given my code 
- * to any student.
-   Print your Name here: Faith Fru Nchang.
+ * @author Faith Fru Nchang.
 */
 public class Alcohol extends Beverage{
    private boolean isWeekend;
