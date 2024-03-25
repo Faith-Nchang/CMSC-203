8cc9ccfd6fd771b7fdabe30a9fedca2d5d856101
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2024-03-24 21:03:54 -0400

    Update Property.java

--- Assignment_4/Property.java
+++ Assignment_4/Property.java
@@ -1,14 +1,6 @@
 /*
- * Class: CMSC203 
- * Instructor: Grigoriy Grinberg
  * Description: a blueprint of the properties in the management company
- * Due: 10/23/2023
- * Platform/compiler: Eclipse
- * I pledge that I have completed the programming 
- * assignment independently. I have not copied the code 
- * from a student or any source. I have not given my code 
- * to any student.
-   Print your Name here: Faith Fru Nchang
+ * @author Faith Fru Nchang
 */
 
 public class Property {
